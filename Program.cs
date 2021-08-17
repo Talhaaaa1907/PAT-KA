b4a4c46eb4d4606cd733058608b4011688fbc871
Talhaa1907 <talhafener680@gmail.com>
2021-08-17 18:26:46 +0300

    PATİKA PROJE

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PATİKA_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adınızı Giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Yaşınızı Giriniz:");
+            string age = Console.ReadLine();
+            Console.WriteLine("Adınız: " +name +" " + "Soyadınız: "+ surname +" " + "Yaşınız: "+age);
+            Console.ReadLine();
+            
+        }
+    }
+}
